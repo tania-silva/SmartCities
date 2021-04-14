c49fb2928ebb8f32b4343a9ad28271cb06fe4d98
tania-silva <61155949+tania-silva@users.noreply.github.com>
2021-04-14 19:33:37 +0100

    Shared Preferences Logado

--- app/src/main/java/com/example/smartcities/MainActivity.kt
+++ app/src/main/java/com/example/smartcities/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.smartcities
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.smartcities.DataClass.Place
@@ -26,24 +29,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val request = ServiceBuilder.buildService(EndPoints::class.java)
-        val call = request.postUtl("stania@ipvc.pt", "1234")
+        val sharedPref: SharedPreferences = getSharedPreferences(
+                getString(R.string.login_p), Context.MODE_PRIVATE
+        )
+        if (sharedPref != null){
+            if(sharedPref.all[getString(R.string.login_shared)]==true){
+                /*var intent = Intent(this, MapsActivity::class.java)
+                startActivity(intent)*/
 
-        call.enqueue(object : Callback<List<OutputPost>>{
-            override fun onResponse(call: Call<List<OutputPost>>, response: Response<List<OutputPost>>) {
-                if (response.isSuccessful){
-                    for(OutputPost in response.body()!!){
-
-                        Log.d("TAG_", OutputPost.email.toString() + OutputPost.pass.toString())
-                    }
-                }
             }
-
-            override fun onFailure(call: Call<List<OutputPost>>, t: Throwable) {
-                Toast.makeText(this@MainActivity, "${t.message}", Toast.LENGTH_SHORT).show()
-            }
-        })
-
+        }
 
 
 
@@ -52,13 +47,75 @@ class MainActivity : AppCompatActivity() {
 
 
     fun button(view: View) {
-       /* val intent = Intent(this, MainActivity2::class.java).apply {
+        val intent = Intent(this, MainActivity2::class.java).apply {
 
         }
-        startActivity(intent)*/
+        startActivity(intent)
+
+    }
+
+
+    fun login(view: View) {
+
+        var email = findViewById<EditText>(R.id.email)
+        var pass = findViewById<EditText>(R.id.pass)
+        val intent = Intent(this, MainActivity2::class.java)
+
+
+ //Validações dos campos email e pass
+        if(email.text.isNullOrEmpty() || pass.text.isNullOrEmpty()){
+
+                if(email.text.isNullOrEmpty()){
+                    email.error = getString(R.string.aviso_email)
+                }
+                if(pass.text.isNullOrEmpty()){
+                    pass.error = getString(R.string.aviso_pass)
+                }
+
+        }else{
+                val request = ServiceBuilder.buildService(EndPoints::class.java)
+                val call = request.postUtl(email.text.toString(), pass.text.toString())
+
+                call.enqueue(object : Callback<List<OutputPost>>{
+                    override fun onResponse(call: Call<List<OutputPost>>, response: Response<List<OutputPost>>) {
+                        if (response.isSuccessful){
+
+                            for(OutputPost in response.body()!!){
+                                // Log.d("TAG_", OutputPost.email.toString() + OutputPost.pass.toString())
+
+                                //Shared Preferences Login
+                                val sharedPref: SharedPreferences = getSharedPreferences(
+                                        getString(R.string.login_p), Context.MODE_PRIVATE
+                                )
+                                with(sharedPref.edit()){
+                                    putBoolean(getString(R.string.login_shared), true)
+                                    putString(getString(R.string.nome), OutputPost.nome)
+                                    putInt(getString(R.string.id_utl), OutputPost.id_utl)
+                                    commit()
+                                    Log.d("****SHARED","${OutputPost.id_utl}" )
+                                }
+
+                            }
+
+
+
+
+
+                            startActivity(intent)
+                        }
+                    }
+
+                    override fun onFailure(call: Call<List<OutputPost>>, t: Throwable) {
+                        Toast.makeText(this@MainActivity, getString(R.string.pass_email_erro), Toast.LENGTH_SHORT).show()
+                    }
+                })
+
+        }
+
+
+
 
 
 
     }
-
 }
--- app/src/main/java/com/example/smartcities/api/OutputPost.kt
+++ app/src/main/java/com/example/smartcities/api/OutputPost.kt
@@ -3,4 +3,6 @@ package com.example.smartcities.api
 class OutputPost (
    val email: String,
    val pass: String,
+   val id_utl: Int,
+   val nome: String,
 )
